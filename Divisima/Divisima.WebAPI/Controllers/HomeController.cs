6d9c82ba440f4ca15b359fbc40bd2d43d4816d0d
Ağacan <agcannn@gmail.com>
2023-05-30 15:09:59 +0300

    update
    api

---
+++ Divisima/Divisima.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Divisima.BL.Repositories;
+using Divisima.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Divisima.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        IRepository<Brand> repoBrand;
+
+        public HomeController(IRepository<Brand> _repoBrand)
+        {
+            repoBrand = _repoBrand;
+        }
+
+
+        //[HttpGet]
+        //public string GetDate()
+        //{
+        //    return DateTime.Now.ToString();
+        //}
+
+        [HttpGet]
+        public IEnumerable<Brand> GetBrands()
+        {
+            return repoBrand.GetAll().OrderByDescending(o => o.Id);
+        }
+
+        [HttpGet("{id}")]
+        public Brand GetBrand(int id)
+        {
+            return repoBrand.GetBy(x => x.Id == id);
+        }
+        /// <summary>
+        /// Burası Marka Ekleme 
+        /// </summary>
+        /// <param name="brand"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<string> Add(Brand brand)
+        {
+            try
+            {
+                await repoBrand.Add(brand);
+                return brand.Name + " Başarıyla eklendi..";
+            }
+            catch (Exception ex)
+            {
+                return $"Marka eklenemedi hata mesajı : {ex.Message} ";
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<string> Delete(int id)
+        {
+            try
+            {
+                var brand = repoBrand.GetBy(x => x.Id == id);
+                await repoBrand.Delete(brand);
+                return brand.Name + " Başarıyla silindi..";
+            }
+            catch (Exception ex)
+            {
+                return $"Marka silinemedi hata mesajı : {ex.Message} ";
+                throw;
+            }
+        }
+
+
+        [HttpPut]
+        public async Task<string> Update(Brand brand)
+        {
+            try
+            {
+                await repoBrand.Update(brand);
+                return brand.Name + " Başarıyla güncellendi..";
+            }
+            catch (Exception ex)
+            {
+                return $"Marka güncellenemedi hata mesajı : {ex.Message} ";
+                throw;
+            }
+        }
+
+    }
+}
--- Divisima/Divisima.WebAPI/Program.cs
+++ Divisima/Divisima.WebAPI/Program.cs
@@ -1,11 +1,38 @@
+using Divisima.BL.Repositories;
+using Divisima.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<SqlContext>(options =>
+          options.UseSqlServer(builder.Configuration.GetConnectionString("CS1")));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(SqlRepository<>));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(sw =>
+{
+    sw.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Divisima Web API - Versiyon 1",
+        Description = "Bu proje .net core 7.0 ile geliþtirilmiþtir",
+        TermsOfService = new Uri("http://www.www.cantacim.com/sozlesme"),
+        Contact = new OpenApiContact
+        {
+            Name = "Aðacan Ergün",
+            Email = "agcannn@gmail.com"
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+        }
+    });
+    sw.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, Assembly.GetExecutingAssembly().GetName().Name + ".xml"));
+});
 
 var app = builder.Build();
 
